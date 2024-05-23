c6f4806db41ddaada2dfa8f955f9414e9c03a0b2
Miguel Mier <miguel.mier@babelgroup.com>
2024-05-23 10:12:40 +0200

    commit inicial (spring initializr)

---
+++ src/main/java/com/michu/player/ComparadorDeJugadoresApplication.java
@@ -0,0 +1,13 @@
+package com.michu.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparadorDeJugadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparadorDeJugadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michu/player/ComparadorDeJugadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michu.player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparadorDeJugadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
